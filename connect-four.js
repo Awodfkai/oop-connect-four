bd9a9a01ff848bad0c03e1b548d849cf0e8fe370
Brian Wang <brbwang@yahoo.com>
2020-08-13 13:02:57 -0400

    newgame

--- connect-four.js
+++ connect-four.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", () =>
     //to keep track of player turn
     let playerTurn = "black";
     let boardState = [[],[],[],[],[],[],[]]
+    let game = undefined;
+
+    
 
 
     //functions
@@ -18,6 +21,16 @@ document.addEventListener("DOMContentLoaded", () =>
         board.classList.add(playerTurn);
     }
 
+    function enableNewGame(){
+        let p1input = document.getElementById("player-1-name");
+        let p2input = document.getElementById("player-2-name");
+        if(p1input.value && p2input.value){
+            document.getElementById("new-game").disabled = false;
+        }else{
+            document.getElementById("new-game").disabled = true;
+        }
+    }
+
     function move(e){
         const t = e.target;
         //get last digit for column number
@@ -28,6 +41,17 @@ document.addEventListener("DOMContentLoaded", () =>
         newToken.classList.add(playerTurn, "token");
         const square = document.getElementById(`square-${rowNumber}-${columnNumber}`);
         square.appendChild(newToken)
+    }
+
+    function newGame(){
+        let p1name = document.getElementById("player-1-name");
+        let p2name = document.getElementById("player-2-name");
+        game = new Game(p1name.value, p2name.value);
+
+        p1name.value = "";
+        p2name.value = "";
+        document.getElementById("new-game").disabled=true;
+        updateUI();
     }
 
 
@@ -42,5 +66,9 @@ document.addEventListener("DOMContentLoaded", () =>
         changePlayerTurn();
     })
 
-    
+    document.getElementById("player-1-name").addEventListener("keyup", enableNewGame);
+    document.getElementById("player-2-name").addEventListener("keyup", enableNewGame);
+
+    document.getElementById("new-game").addEventListener("click", newGame);
+
 })
